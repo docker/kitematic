691b766ec59b0f7bdf77a4a99f7bd49cedbb6734
Jeff Morgan <jmorganca@gmail.com>
2014-09-10 23:03:44 -0700

    Fixing HEAD in index.js

--- index.js
+++ index.js
@@ -47,11 +47,7 @@ var start = function (callback) {
         console.log('MongoDB: ' + mongoPort);
         console.log('webPort: ' + webPort);
         child_process.exec('kill $(ps aux -e | grep PURPOSE=KITEMATIC | awk \'{print $2}\') && rm ' + path.join(dataPath, 'mongod.lock'), function (error, stdout, stderr) {
-<<<<<<< HEAD
           var mongoChild = child_process.spawn(path.join(__dirname, 'resources', 'mongod'), ['--bind_ip', '127.0.0.1', '--dbpath', dataPath, '--port', mongoPort, '--unixSocketPrefix', dataPath], {
-=======
-          var mongoChild = child_process.spawn(path.join(process.cwd(), 'resources', 'mongod'), ['--bind_ip', '127.0.0.1', '--dbpath', dataPath, '--port', mongoPort, '--unixSocketPrefix', dataPath], {
->>>>>>> master
             env: {
               PURPOSE: 'KITEMATIC'
             }
