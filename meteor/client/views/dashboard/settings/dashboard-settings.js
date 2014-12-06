0bb1153103d468f76c50c919017aee5090d31ac0
Jeff Morgan <jmorganca@gmail.com>
2014-12-06 14:41:59 -0500

    Initial implementation of mixpanel instead of GA

--- meteor/client/views/dashboard/settings/dashboard-settings.js
+++ meteor/client/views/dashboard/settings/dashboard-settings.js
@@ -2,60 +2,22 @@ var remote = require('remote');
 var dialog = remote.require('dialog');
 
 Template.dashboardSettings.events({
-  'click .btn-start-boot2docker': function (e) {
-    var $btn = $(e.currentTarget);
-    $btn.html('Starting Boot2Docker...');
-    $btn.attr("disabled", "disabled");
-    Session.set('boot2dockerOff', false);
-    Boot2Docker.start(function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  },
-  'click .btn-stop-boot2docker': function (e) {
-    var $btn = $(e.currentTarget);
-    $btn.html('Stopping Boot2Docker...');
-    $btn.attr("disabled", "disabled");
-    Session.set('boot2dockerOff', true);
-    Boot2Docker.stop(function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  },
   'click .btn-usage-analytics-on': function () {
-    var settings = Settings.findOne();
-    Settings.update(settings._id, {
-      $set: {
-        tracking: true
-      }
-    });
+    localStorage.setItem('metrics.enabled', true);
+    Session.set('metrics.enabled', true);
   },
   'click .btn-usage-analytics-off': function () {
-    var settings = Settings.findOne();
-    Settings.update(settings._id, {
-      $set: {
-        tracking: false
-      }
-    });
-  },
-  'click .btn-repair': function () {
-    dialog.showMessageBox({
-      message: 'Repairing Kitematic will clear your current Docker VM and the state of the app. Please make sure your work is backed up. Do you wish to continue?',
-      buttons: ['Repair', 'Cancel']
-    }, function (index) {
-      if (index !== 0) {
-        return;
-      }
-      Router.go('setup_intro');
-    });
+    localStorage.setItem('metrics.enabled', false);
+    Session.set('metrics.enabled', false);
   }
 });
 
 Template.dashboardSettings.helpers({
-  settings: function () {
-    return Settings.findOne({});
+  metricsEnabled: function () {
+    if (Session.get('metrics.enabled') === undefined) {
+      Session.set('metrics.enabled', localStorage.getItem('metrics.enabled'));
+    }
+    return Session.get('metrics.enabled');
   },
   memory: function () {
     return Session.get('boot2dockerMemoryUsage');
