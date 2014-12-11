f1d1f9e15509b40fa3f76ca9e30b453d16999732
Jeff Morgan <jmorganca@gmail.com>
2014-12-10 17:29:37 -0800

    Changing to leveldb

--- meteor/client/lib/metrics.js
+++ meteor/client/lib/metrics.js
@@ -1,8 +1,10 @@
 var remote = require('remote');
 var app = remote.require('app');
 var crypto = require('crypto');
-var getmac = require('getmac');
 var uuid = require('node-uuid');
+var level = require('levelup');
+var path = require('path');
+var db = level(path.join(process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'], 'Library/Application Support/Kitematic/data', 'db'));
 
 Metrics = {};
 
@@ -11,24 +13,28 @@ Metrics.trackEvent = function (name) {
     return;
   }
   var uuid = localStorage.getItem('metrics.uuid');
-  if (localStorage.getItem('metrics.enabled') && uuid) {
-    mixpanel.track('docker_gui ' + name, {
-      distinct_id: uuid,
-      version: app.getVersion()
-    });
-  }
+  db.get('metrics.enabled', function (err, value) {
+    if (!err && uuid) {
+      mixpanel.track('docker_gui ' + name, {
+        distinct_id: uuid,
+        version: app.getVersion()
+      });
+    }
+  });
 };
 
 Metrics.prepareTracking = function () {
-  if (localStorage.getItem('metrics.enabled') === null) {
-    var settings = Settings.findOne();
-    if (settings && settings.tracking) {
-      localStorage.setItem('metrics.enabled', !!settings.tracking);
-    } else {
-      localStorage.setItem('metrics.enabled', true);
+  db.get('metrics.enabled', function (err, value) {
+    if (err && err.notFound) {
+      var settings = Settings.findOne();
+      if (settings && settings.tracking) {
+        db.put('metrics.enabled', !!settings.tracking);
+      } else {
+        db.put('metrics.enabled', true);
+      }
     }
-  }
-  if (!localStorage.getItem('metrics.uuid')) {
-    localStorage.setItem('metrics.uuid', uuid.v4());
-  }
+    db.get('metrics.uuid', function (err, value) {
+      db.put('metrics.uuid', uuid.v4());
+    });
+  });
 };
