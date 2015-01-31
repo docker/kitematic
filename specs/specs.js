cb4488692532dffecf0840c7036188b1ffd86fd3
Jeffrey Morgan <jmorganca@gmail.com>
2015-01-31 01:01:06 -0500

    Fixing tests

--- specs/specs.js
+++ specs/specs.js
@@ -1,10 +1,16 @@
 window.jasmineRequire = require('./jasmine-2.1.3/jasmine');
 require('./jasmine-2.1.3/jasmine-html');
 require('./jasmine-2.1.3/boot');
+var consoleReporter = require('./jasmine-2.1.3/console');
 
-var Reporter = require('jasmine-terminal-reporter');
-var reporter = new Reporter();
-jasmine.getEnv().addReporter(reporter);
+jasmine.getEnv().addReporter(new consoleReporter.ConsoleReporter()({
+    showColors: true,
+    timer: new jasmine.Timer(),
+    print: function() {
+      console.log(arguments);
+      process.stdout.write.apply(process.stdout, arguments);
+    }
+  }));
 
 var fs = require('fs');
 var tests = fs.readdirSync('./specs').filter(function (f) {
