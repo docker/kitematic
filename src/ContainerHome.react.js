c54792ea2604428856f9d4a038ec5b59ca75e55c
Sean Li <mail@shang.li>
2015-02-04 13:24:07 -0800

    Home page skeleton.

--- src/ContainerHome.react.js
+++ src/ContainerHome.react.js
@@ -1,16 +1,36 @@
+var $ = require('jquery');
 var React = require('react/addons');
 
 var ContainerHome = React.createClass({
+  componentDidUpdate: function () {
+    // Scroll logs to bottom
+    var parent = $('.mini-logs');
+    if (parent.length) {
+      if (parent.scrollTop() >= this._oldHeight) {
+        parent.stop();
+        parent.scrollTop(parent[0].scrollHeight - parent.height());
+      }
+      this._oldHeight = parent[0].scrollHeight - parent.height();
+    }
+  },
   render: function () {
-    /*var preview;
-    if (this.state.defaultPort) {
+    var preview;
+    if (this.props.defaultPort) {
       preview = (
-        <iframe src={this.state.ports[this.state.defaultPort].url} autosize="on"></iframe>
+        <iframe src={this.props.ports[this.props.defaultPort].url}></iframe>
       );
-    }*/
+    }
     return (
-      <div className="details-panel">
-        <h1>HOME</h1>
+      <div className="details-panel home">
+        <div className="content">
+          <h4>Web Preview</h4>
+          <div className="widget web-preview">
+            {preview}
+          </div>
+          <div className="widget mini-logs">
+            {this.props.logs}
+          </div>
+        </div>
       </div>
     );
   }
