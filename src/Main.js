f401c97001f6aa9e0c0bdd76db9d087eab74c77d
Sean Li <mail@shang.li>
2015-02-24 12:23:49 -0800

    Fixed cannot backspace when form is focused.

--- src/Main.js
+++ src/Main.js
@@ -51,7 +51,18 @@ Menu.setApplicationMenu(menu);
 
 document.onkeydown = function (e) {
   e = e || window.event;
+  var doPrevent;
   if (e.keyCode === 8) {
+    var d = e.srcElement || e.target;
+    if (d.tagName.toUpperCase() === 'INPUT' || d.tagName.toUpperCase() === 'TEXTAREA') {
+      doPrevent = d.readOnly || d.disabled;
+    } else {
+      doPrevent = true;
+    }
+  } else {
+    doPrevent = false;
+  }
+  if (doPrevent) {
     e.preventDefault();
   }
 };
