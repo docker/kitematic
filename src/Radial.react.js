f8781f1ba26f9746580623fb508b9db89af548e9
Sean Li <mail@shang.li>
2015-02-26 16:30:30 -0800

    Display 0% for progress bar and CSS tweak.

--- src/Radial.react.js
+++ src/Radial.react.js
@@ -3,7 +3,7 @@ var React = require('react/addons');
 var Radial = React.createClass({
   render: function () {
     var percentage;
-    if (this.props.progress && !this.props.spin) {
+    if ((this.props.progress !== null && this.props.progress !== undefined) && !this.props.spin) {
       percentage = (
         <div className="percentage"></div>
       );
