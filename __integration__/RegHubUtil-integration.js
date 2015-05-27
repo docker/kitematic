a5d674aa4ff8c4887212ddab7e93579209a7236c
Jeffrey Morgan <jmorganca@gmail.com>
2015-05-27 14:31:19 -0700

    Fix lowercase typo

--- __integration__/RegHubUtil-integration.js
+++ __integration__/RegHubUtil-integration.js
@@ -1,11 +1,10 @@
-jest.autoMockOff();
 
 // One minute timeout for integration tests
 jasmine.getEnv().DEFAULT_TIMEOUT_INTERVAL = 60000;
 
 let _ = require('underscore');
 let regHubUtil = require('../src/utils/RegHubUtil');
-let hubUtil = require('../src/utils/hubUtil');
+let hubUtil = require('../src/utils/HubUtil');
 let Promise = require('bluebird');
 
 describe('RegHubUtil Integration Tests', () => {
