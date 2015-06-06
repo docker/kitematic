dde3fabfc53798cfd923ea8ab616a5845929a45a
Vojta Bartos <me@vojtechbartos.cz>
2015-06-06 16:34:52 +0200

    Added port validation at container port settings
    - expecting integer
    - needs to be in range <1, 2^16>
    - no port collision with other containers ports
    - no port collision with inside container

--- src/components/ContainerSettingsPorts.react.js
+++ src/components/ContainerSettingsPorts.react.js
@@ -2,6 +2,7 @@ var _ = require('underscore');
 var React = require('react/addons');
 var shell = require('shell');
 var containerActions = require('../actions/ContainerActions');
+var containerStore = require('../stores/ContainerStore');
 var ContainerUtil = require('../utils/ContainerUtil');
 var metrics = require('../utils/MetricsUtil');
 
@@ -20,12 +21,38 @@ var ContainerSettingsPorts = React.createClass({
     });
     shell.openExternal(url);
   },
-  handleChangePort: function(key, e) {;
+  handleChangePort: function(key, e) {
     var ports = this.state.ports;
+    var port = e.target.value;
+
+    // save updated port
     ports[key] = _.extend(ports[key], {
-      url: 'http://' + ports[key]['ip'] + ':' + e.target.value,
-      port: e.target.value
+      url: 'http://' + ports[key]['ip'] + ':' + port,
+      port: port,
+      error: null
     });
+
+    // basic validation, if number is integer, if its in range, if there
+    // is no collision with ports of other containers and also if there is no
+    // collision with ports for current containser
+    const name = this.props.container.Name;
+    const containers = containerStore.getState().containers;
+    const container = ContainerUtil.isPortCollision(name, containers, port);
+    const duplicates = _.filter(ports, (v, i) => {
+      return (i != key && _.isEqual(v.port, port));
+    });
+    if (!port.match(/^[0-9]+$/g)) {
+      ports[key].error = 'Needs to be an integer.';
+    }
+    else if (port <= 0 || port > 65535) {
+      ports[key].error = 'Needs to be in range <1,65535>.';
+    }
+    else if (container) {
+      ports[key].error = 'Collision with port at container "'+ container.Name +'"';
+    }
+    else if (duplicates.length > 0) {
+      ports[key].error = "Collision with other port at container.";
+    }
 
     this.setState({ ports: ports });
   },
@@ -46,11 +73,13 @@ var ContainerSettingsPorts = React.createClass({
     if (!this.props.container) {
       return (<div></div>);
     }
+    var isUpdating = (this.props.container.State.Updating);
+    var isValid = true;
     var ports = _.map(_.pairs(this.state.ports), pair => {
       var key = pair[0];
-      var ip = pair[1].ip;
-      var port = pair[1].port;
-      var url = pair[1].url;
+      var {ip, port, url, error} = pair[1];
+      isValid = (error) ? false : isValid;
+
       return (
         <div key={key} className="table-values">
           <span className="value-left">{key}</span>
@@ -62,6 +91,7 @@ var ContainerSettingsPorts = React.createClass({
               className="line"
               onChange={this.handleChangePort.bind(this, key)}
               defaultValue={port}></input>
+            <span className="error">{error}</span>
           </span>
         </div>
       );
@@ -78,7 +108,7 @@ var ContainerSettingsPorts = React.createClass({
             {ports}
           </div>
           <a className="btn btn-action"
-             disabled={this.props.container.State.Updating}
+             disabled={isUpdating || !isValid}
              onClick={this.handleSave}>
             Save
           </a>
