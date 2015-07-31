cf8883ba288ab6fc83b2e80888d85315079eac19
Tim Brandin <tim.brandin@gmail.com>
2015-07-31 15:27:26 +0200

    Added support for kitematic open

--- cli/bin/kitematic.js
+++ cli/bin/kitematic.js
@@ -5,31 +5,38 @@ var cjson = require('cjson');
 var Config = require('../lib/config');
 var ActionsRegistry = require('../lib/actions');
 var helpers = require('../lib/helpers');
+var shell = require("shelljs");
 require('colors');
 
-console.log('\nKitematic: The easiest way to start using Docker on Mac & Windows'.bold.blue);
-console.log('-----------------------------------------------------------------\n'.bold.blue);
-
 var action = process.argv[2];
-if(action == 'init') {
-  //special setup for init
-  ActionsRegistry.init();
-} else {
-  var cwd = path.resolve('.');
-  //read config and validate it
-  var config = Config.read();
-  runActions(config, cwd);
-}
 
-function runActions(config, cwd) {
-  var actionsRegistry = new ActionsRegistry(config, cwd);
-  if(actionsRegistry[action]) {
-    actionsRegistry[action]();
+if (action != 'open') {
+  console.log('\nKitematic: The easiest way to start using Docker on Mac & Windows'.bold.blue);
+  console.log('-----------------------------------------------------------------\n'.bold.blue);
+
+  if(action == 'init') {
+    //special setup for init
+    ActionsRegistry.init();
   } else {
-    if(typeof action !== "undefined") {
-      var errorMessage = 'No Such Action Exists: ' + action;
-      console.error(errorMessage.bold.red);
-    }
-    helpers.printHelp();
+    var cwd = path.resolve('.');
+    //read config and validate it
+    var config = Config.read();
+    runActions(config, cwd);
   }
+
+  function runActions(config, cwd) {
+    var actionsRegistry = new ActionsRegistry(config, cwd);
+    if(actionsRegistry[action]) {
+      actionsRegistry[action]();
+    } else {
+      if(typeof action !== "undefined") {
+        var errorMessage = 'No Such Action Exists: ' + action;
+        console.error(errorMessage.bold.red);
+      }
+      helpers.printHelp();
+    }
+  }
+}
+else {
+  shell.exec('open -a "Kitematic (Beta)"');
 }
