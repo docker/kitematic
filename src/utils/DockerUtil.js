636c1d4590083d3d3a16c4306896122353410680
FrenchBen <me@frenchben.com>
2015-08-27 14:18:46 -0400

    Updated Container restart
    Signed-off-by: FrenchBen <me@frenchben.com>

--- src/utils/DockerUtil.js
+++ src/utils/DockerUtil.js
@@ -262,17 +262,17 @@ export default {
   },
 
   restart (name) {
-    let container = this.client.getContainer(name);
-    container.stop(error => {
-      if (error && error.statusCode !== 304) {
-        containerServerActions.error({name, error});
+    this.client.getContainer(name).stop(stopError => {
+      if (stopError && stopError.statusCode !== 304) {
+        containerServerActions.error({name, stopError});
         return;
       }
-      container.inspect((error, data) => {
-        if (error) {
-          containerServerActions.error({name, error});
+      this.client.getContainer(name).start(startError => {
+        if (startError && startError.statusCode !== 304) {
+          containerServerActions.error({name, startError});
+          return;
         }
-        this.startContainer(name, data);
+        this.fetchContainer(name);
       });
     });
   },
