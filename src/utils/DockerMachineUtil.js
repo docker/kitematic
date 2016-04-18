5fb1693e432fc01d795314a039f057aeb8cbce32
Mario Pietsch <pmariojo@gmail.com>
2016-04-18 18:51:55 +0200

    format args template string

--- src/utils/DockerMachineUtil.js
+++ src/utils/DockerMachineUtil.js
@@ -62,9 +62,9 @@ var DockerMachine = {
     //TODO: check options elements!
     switch (provider){
         case "hyperv":{
-          console.log('started create with hyper-v');
+          // console.log('started create with hyper-v');
           let virtualSwitch = localStorage.getItem('virtualSwitch');
-          console.log('will use this switch ' + virtualSwitch + ' with an existing switch')
+          // console.log('will use this switch ' + virtualSwitch + ' with an existing switch')
 
           let args= `"` + `docker-machine.exe -D create --driver hyperv --hyperv-memory 2048 `+
                     `--hyperv-virtual-switch '${virtualSwitch}' ${machineName}` + `"`;
@@ -72,14 +72,13 @@ var DockerMachine = {
           return powershellUtil.runCommandWithArgs(args);
         }
         default: {
-          console.log("virtualboxInstalled: " + virtualBoxIsInstalled);
+          // console.log("virtualboxInstalled: " + virtualBoxIsInstalled);
           if (virtualBoxIsInstalled === true) {
-            console.log('started create with virtualbox');
-            
-            let args= `"docker-machine.exe -D create --driver virtualbox --virtualbox-memory 2048 ` + machineName + `"`;
+            //console.log('started create with virtualbox');
+            let args= `"` + `docker-machine.exe -D create --driver virtualbox --virtualbox-memory 2048 ${machineName}` + `"`;
             return powershellUtil.runCommandWithArgs(args);
           } else {
-            throw new Error('virtualbox not installed, so this cannot continue');
+            throw new Error('virtualbox is not installed. Process stopped!');
           }
         }
     }
