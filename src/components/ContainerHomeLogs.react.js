62746cfe04ae205077146de384c27067b5355287
Collins Abitekaniza <abtcolns@gmail.com>
2017-12-08 00:12:54 +0300

    3223 Font too small and can't copy log. (#3369)
    * add logs to clipboard when copy button is pressed
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * remove ansi codes from logs
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * enable adjusting font size for logs
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * style font size select
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * change btn text to copied on copy logs
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * remove box border from both buttons
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>
    
    * change Copied ! btn text color to white

--- src/components/ContainerHomeLogs.react.js
+++ src/components/ContainerHomeLogs.react.js
@@ -3,6 +3,7 @@ import React from 'react/addons';
 import Router from 'react-router';
 import containerActions from '../actions/ContainerActions';
 import Convert from 'ansi-to-html';
+const { clipboard } = require('electron');
 
 let escape = function (html) {
   var text = document.createTextNode(html);
@@ -11,11 +12,40 @@ let escape = function (html) {
   return div.innerHTML;
 };
 
+var FontSelect = React.createClass({
+  
+  getFontSizes: function(start, end){
+    let options = [];
+    for(let i = start; i<=end; i++){
+      options.push(<option key={i} value={i}>{i+' px'}</option>);
+    }
+    return options;
+  },
+
+  render: function(){
+    return (
+      <select className='logs-font-size__select' value={this.props.fontSize} onChange={this.props.onChange}>
+        <option disabled="true" >Font size</option>
+        {this.getFontSizes(10, 30)}
+      </select>
+    );
+  }
+});
+
 let convert = new Convert();
 let prevBottom = 0;
 
 module.exports = React.createClass({
-
+  getInitialState: function(){
+    return {
+      fontSize: 10
+    };
+  },
+  onFontChange: function(event){
+    this.setState({
+      fontSize: event.target.value
+    });
+  },
   componentDidUpdate: function () {
     var node = $('.logs').get()[0];
     node.scrollTop = node.scrollHeight;
@@ -36,18 +66,36 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    let _logs = '';
     let logs = this.props.container.Logs ? this.props.container.Logs.map((l, index) => {
         const key = `${this.props.container.Name}-${index}`;
+        _logs = _logs.concat(escape(l.substr(l.indexOf(' ')+1)).replace(/\[\d+m/g,''));
         return <div key={key} dangerouslySetInnerHTML={{__html: convert.toHtml(escape(l.substr(l.indexOf(' ')+1)).replace(/ /g, '&nbsp;<wbr>'))}}></div>;
       }) : ['0 No logs for this container.'];
+
+    let copyLogs = (event)=>{
+      clipboard.writeText(_logs);
+
+      let btn = event.target;
+      btn.innerHTML = 'Copied !';
+      btn.style.color = '#FFF';
+      setTimeout(()=>{
+        btn.style.color = 'inherit'
+        btn.innerHTML = 'Copy';
+      }, 1000);
+    };
 
     return (
       <div className="mini-logs wrapper">
         <div className="widget">
           <div className="top-bar">
             <div className="text">Container Logs</div>
+            <div>
+                <FontSelect fontSize={this.state.fontSize} onChange={this.onFontChange} />
+                <button className="copy-logs__btn" onClick={copyLogs}>Copy</button>
+            </div>
           </div>
-          <div className="logs">
+          <div className="logs" style={{fontSize:this.state.fontSize+'px'}}>
             {logs}
           </div>
         </div>
