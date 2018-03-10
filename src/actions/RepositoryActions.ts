2a46a7899c1c483bfef0700f649646b15d3fd3bb
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-03-10 22:33:44 +0100

    RepositoryActions

---
+++ src/actions/RepositoryActions.ts
@@ -0,0 +1,23 @@
+import alt from "../alt";
+import regHubUtil from "../utils/RegHubUtil";
+
+class RepositoryActions {
+
+  public recommended() {
+      (this as any).dispatch({});
+      regHubUtil.recommended();
+  }
+
+  public search(query, page = 1) {
+      (this as any).dispatch({query, page});
+      regHubUtil.search(query, page);
+  }
+
+  public repos() {
+      (this as any).dispatch({});
+      regHubUtil.repos();
+  }
+
+}
+
+export default alt.createActions(RepositoryActions);
