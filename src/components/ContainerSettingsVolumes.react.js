04e742bd741e71dbe6bd50bb3e5789316934fa1a
Collins Abitekaniza <abtcolns@gmail.com>
2018-03-26 15:00:53 +0300

    enable adding volumes to containers
    Signed-off-by: Collins Abitekaniza <abtcolns@gmail.com>

--- src/components/ContainerSettingsVolumes.react.js
+++ src/components/ContainerSettingsVolumes.react.js
@@ -9,6 +9,16 @@ import metrics from '../utils/MetricsUtil';
 import containerActions from '../actions/ContainerActions';
 
 var ContainerSettingsVolumes = React.createClass({
+  getInitialState: function() {
+    return {
+      dockerVol: '',
+    }
+  },
+  handleDockerVolChange: function(event) {
+    this.setState({
+      dockerVol: event.target.value
+    });
+  },
   handleChooseVolumeClick: function (dockerVol) {
     dialog.showOpenDialog({properties: ['openDirectory', 'createDirectory']}, (filenames) => {
       if (!filenames) {
@@ -29,12 +39,35 @@ var ContainerSettingsVolumes = React.createClass({
       metrics.track('Choose Directory for Volume');
 
       let mounts = _.clone(this.props.container.Mounts);
+      let match = false;
       _.each(mounts, m => {
         if (m.Destination === dockerVol) {
           m.Source = util.windowsToLinuxPath(directory);
           m.Driver = null;
+          match = true;
         }
       });
+
+      // if this is a new volume
+      if (!match) {
+        if (!dockerVol){
+          metrics.track('Added Volume Directory', {
+            from: 'settings'
+          });
+          dialog.showMessageBox({
+            type: 'error',
+            buttons: ['OK'],
+            message: 'Invalid docker volume path'
+          });
+          return;
+        }
+        mounts.push({
+          Destination: dockerVol,
+          Source: util.windowsToLinuxPath(directory),
+          Driver: null,
+        });
+        this.setState({ dockerVol: '' })
+      }
 
       let binds = mounts.map(m => {
         return m.Source + ':' + m.Destination;
@@ -82,14 +115,30 @@ var ContainerSettingsVolumes = React.createClass({
     }
 
     var homeDir = util.isWindows() ? util.windowsToLinuxPath(util.home()) : util.home();
-    var mounts = _.map(this.props.container.Mounts, (m, i) => {
+    var mounts = _.map(_.union(this.props.container.Mounts, [['','']]), (m, i) => {
       let source = m.Source, destination = m.Destination;
+      let icons;
+
+      if (!destination) {
+        destination = <input type="text" value={this.state.dockerVol} className="key line" onChange={this.handleDockerVolChange}></input>
+      }
       if (!m.Source || (!util.isNative() && m.Source.indexOf(homeDir) === -1) || (m.Source.indexOf('/var/lib/docker/volumes') !== -1)) {
+        icons =(
+          <div>
+            <a className="only-icon btn btn-positive small" disabled={this.props.container.State.Updating} onClick={this.handleChooseVolumeClick.bind(this, this.state.dockerVol)}><span className="icon icon-add"></span></a>
+          </div>
+        )
         source = (
           <span className="value-right">No Folder</span>
         );
       } else {
         let local = util.isWindows() ? util.linuxToWindowsPath(source) : source;
+        icons = ( 
+        <div>
+          <a className="only-icon btn btn-action small"><span className="icon icon-delete"></span></a>
+          <a className="btn btn-action small" disabled={this.props.container.State.Updating} onClick={this.handleChooseVolumeClick.bind(this, destination)}>Change</a>
+        </div>
+      );
         source = (
           <a className="value-right" onClick={this.handleOpenVolumeClick.bind(this, source)}>{local.replace(process.env.HOME, '~')}</a>
         );
@@ -99,8 +148,7 @@ var ContainerSettingsVolumes = React.createClass({
           <td>{destination}</td>
           <td>{source}</td>
           <td>
-            <a className="btn btn-action small" disabled={this.props.container.State.Updating} onClick={this.handleChooseVolumeClick.bind(this, destination)}>Change</a>
-            <a className="btn btn-action small" disabled={this.props.container.State.Updating} onClick={this.handleRemoveVolumeClick.bind(this, destination)}>Remove</a>
+            {icons}
           </td>
         </tr>
       );
