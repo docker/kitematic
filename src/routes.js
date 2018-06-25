3e08bf7933f77a5c004eb4ba99127c51a4d70da6
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-06-25 19:33:56 +0200

    routes

--- src/routes.js
+++ src/routes.js
@@ -18,42 +18,42 @@ import Loading from './components/Loading.react';
 import NewContainerSearch from './components/NewContainerSearch.react';
 import Router from 'react-router';
 
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
-var RouteHandler = Router.RouteHandler;
+const Route = Router.Route;
+const DefaultRoute = Router.DefaultRoute;
+const RouteHandler = Router.RouteHandler;
 
-var App = React.createClass({
-  render: function () {
-    return (
-      <RouteHandler/>
-    );
-  }
+const App = React.createClass({
+	render: function () {
+		return (
+			<RouteHandler/>
+		);
+	}
 });
 
-var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="account" path="account" handler={Account}>
-      <Route name="signup" path="signup" handler={AccountSignup}/>
-      <Route name="login" path="login" handler={AccountLogin}/>
-    </Route>
-    <Route name="containers" path="containers" handler={Containers}>
-      <Route name="container" path="details/:name" handler={ContainerDetails}>
-        <DefaultRoute name="containerHome" handler={ContainerHome} />
-        <Route name="containerSettings" path="settings" handler={ContainerSettings}>
-          <Route name="containerSettingsGeneral" path="general" handler={ContainerSettingsGeneral}/>
-          <Route name="containerSettingsPorts" path="ports" handler={ContainerSettingsPorts}/>
-          <Route name="containerSettingsVolumes" path="volumes" handler={ContainerSettingsVolumes}/>
-          <Route name="containerSettingsNetwork" path="network" handler={ContainerSettingsNetwork}/>
-          <Route name="containerSettingsAdvanced" path="advanced" handler={ContainerSettingsAdvanced}/>
-        </Route>
-      </Route>
-      <Route name="search" handler={NewContainerSearch}/>
-      <Route name="preferences" path="preferences" handler={Preferences}/>
-      <Route name="about" path="about" handler={About}/>
-    </Route>
-    <DefaultRoute name="loading" handler={Loading}/>
-    <Route name="setup" handler={Setup}/>
-  </Route>
+const routes = (
+	<Route name="app" path="/" handler={App}>
+		<Route name="account" path="account" handler={Account}>
+			<Route name="signup" path="signup" handler={AccountSignup}/>
+			<Route name="login" path="login" handler={AccountLogin}/>
+		</Route>
+		<Route name="containers" path="containers" handler={Containers}>
+			<Route name="container" path="details/:name" handler={ContainerDetails}>
+				<DefaultRoute name="containerHome" handler={ContainerHome}/>
+				<Route name="containerSettings" path="settings" handler={ContainerSettings}>
+					<Route name="containerSettingsGeneral" path="general" handler={ContainerSettingsGeneral}/>
+					<Route name="containerSettingsPorts" path="ports" handler={ContainerSettingsPorts}/>
+					<Route name="containerSettingsVolumes" path="volumes" handler={ContainerSettingsVolumes}/>
+					<Route name="containerSettingsNetwork" path="network" handler={ContainerSettingsNetwork}/>
+					<Route name="containerSettingsAdvanced" path="advanced" handler={ContainerSettingsAdvanced}/>
+				</Route>
+			</Route>
+			<Route name="search" handler={NewContainerSearch}/>
+			<Route name="preferences" path="preferences" handler={Preferences}/>
+			<Route name="about" path="about" handler={About}/>
+		</Route>
+		<DefaultRoute name="loading" handler={Loading}/>
+		<Route name="setup" handler={Setup}/>
+	</Route>
 );
 
 module.exports = routes;
