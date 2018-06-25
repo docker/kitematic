06bd8d473664372b16f22c3dfc41049203d04867
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-06-25 18:18:19 +0200

    browser.ts

--- src/browser.ts
+++ src/browser.ts
@@ -1,25 +1,26 @@
-import { Promise } from "bluebird";
-import { app, BrowserWindow } from "electron";
-import { readFileSync } from "fs";
+import {Promise} from "bluebird";
+import {app, BrowserWindow} from "electron";
+import {readFileSync} from "fs";
 import * as os from "os";
 import * as path from "path";
+
 process.env.NODE_PATH = path.join(__dirname, "node_modules");
 process.env.RESOURCES_PATH = path.join(__dirname, "/../resources");
 if (process.platform !== "win32") {
     process.env.PATH = "/usr/local/bin:" + process.env.PATH;
 }
 let exiting = false;
-let size = {};
-let settingsjson = {};
+let size: {height, width} = {} as any;
+let settingsjson: {} = {} as any;
 try {
     size = JSON.parse(readFileSync(path.join(app.getPath("userData"), "size"), "utf8"));
-}
-catch (err) { }
+} catch (err) {}
+
 try {
     settingsjson = JSON.parse(readFileSync(path.join(__dirname, "settings.json"), "utf8"));
-}
-catch (err) { }
-app.on("ready", function () {
+} catch (err) {}
+
+app.on("ready", function() {
     const mainWindow = new BrowserWindow({
         frame: false,
         height: size.height || 680,
@@ -29,48 +30,54 @@ app.on("ready", function () {
         show: false,
         width: size.width || 1080,
     });
+
     if (process.env.NODE_ENV === "development") {
-        mainWindow.webContents.openDevTools({ mode: "detach" });
+        mainWindow.webContents.openDevTools({mode: "detach"});
     }
+
     mainWindow.loadURL(path.normalize("file://" + path.join(__dirname, "index.html")));
-    app.on("activate", function () {
+
+    app.on("activate", function() {
         if (mainWindow) {
             mainWindow.show();
         }
         return false;
     });
+
     if (os.platform() === "win32" || os.platform() === "linux") {
-        mainWindow.on("close", function (e) {
+        mainWindow.on("close", function(e) {
             mainWindow.webContents.send("application:quitting");
             if (!exiting) {
-                Promise.delay(1000).then(function () {
+                Promise.delay(1000).then(function() {
                     mainWindow.close();
                 });
                 exiting = true;
                 e.preventDefault();
             }
         });
-        app.on("window-all-closed", function () {
+
+        app.on("window-all-closed", function() {
             app.quit();
         });
-    }
-    else if (os.platform() === "darwin") {
-        app.on("before-quit", function () {
+    } else if (os.platform() === "darwin") {
+        app.on("before-quit", function() {
             mainWindow.webContents.send("application:quitting");
         });
     }
-    mainWindow.webContents.on("new-window", function (e) {
+
+    mainWindow.webContents.on("new-window", function(e) {
         e.preventDefault();
     });
-    mainWindow.webContents.on("will-navigate", function (e, url) {
+
+    mainWindow.webContents.on("will-navigate", function(e, url) {
         if (url.indexOf("build/index.html#") < 0) {
             e.preventDefault();
         }
     });
-    mainWindow.webContents.on("did-finish-load", function () {
+
+    mainWindow.webContents.on("did-finish-load", function() {
         mainWindow.setTitle("Kitematic");
         mainWindow.show();
         mainWindow.focus();
     });
 });
-//# sourceMappingURL=browser.js.map
