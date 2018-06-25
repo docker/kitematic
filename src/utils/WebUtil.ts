d42ab5a9f61bcfaaf0a30232f904c251bfae0449
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-06-25 21:14:24 +0200

    .

--- src/utils/WebUtil.ts
+++ src/utils/WebUtil.ts
@@ -1,75 +1,75 @@
-import {remote} from 'electron';
+import {remote} from "electron";
 const app = remote.app;
-import * as fs from 'fs';
-import util from './Util';
-import * as path from 'path';
-import bugsnag from 'bugsnag-js';
-import metrics from './MetricsUtil';
+import bugsnag from "bugsnag-js";
+import * as fs from "fs";
+import * as path from "path";
+import metrics from "./MetricsUtil";
+import util from "./Util";
 
 export default {
-  addWindowSizeSaving: function () {
-    window.addEventListener('resize', function () {
-      fs.writeFileSync(path.join(app.getPath('userData'), 'size'), JSON.stringify({
-        width: window.outerWidth,
-        height: window.outerHeight
-      }));
-    });
+  addWindowSizeSaving() {
+	window.addEventListener("resize", function() {
+		fs.writeFileSync(path.join(app.getPath("userData"), "size"), JSON.stringify({
+		width: window.outerWidth,
+		height: window.outerHeight
+		}));
+	});
   },
-  addLiveReload: function () {
-    if (process.env.NODE_ENV === 'development') {
-      var head = document.getElementsByTagName('head')[0];
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'http://localhost:35729/livereload.js';
-      head.appendChild(script);
-    }
+  addLiveReload() {
+	if (process.env.NODE_ENV === "development") {
+		let head = document.getElementsByTagName("head")[0];
+		let script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "http://localhost:35729/livereload.js";
+		head.appendChild(script);
+	}
   },
-  addBugReporting: function () {
-    var settingsjson = util.settingsjson();
+  addBugReporting() {
+	let settingsjson = util.settingsjson();
 
-    if (settingsjson.bugsnag) {
-      bugsnag.apiKey = settingsjson.bugsnag;
-      bugsnag.autoNotify = true;
-      bugsnag.releaseStage = process.env.NODE_ENV === 'development' ? 'development' : 'production';
-      bugsnag.notifyReleaseStages = ['production'];
-      bugsnag.appVersion = app.getVersion();
+	if (settingsjson.bugsnag) {
+		bugsnag.apiKey = settingsjson.bugsnag;
+		bugsnag.autoNotify = true;
+		bugsnag.releaseStage = process.env.NODE_ENV === "development" ? "development" : "production";
+		bugsnag.notifyReleaseStages = ["production"];
+		bugsnag.appVersion = app.getVersion();
 
-      bugsnag.beforeNotify = function(payload) {
-        if (!metrics.enabled()) {
-          return false;
-        }
+		bugsnag.beforeNotify = function(payload) {
+		if (!metrics.enabled()) {
+			return false;
+		}
 
-        payload.stacktrace = util.removeSensitiveData(payload.stacktrace);
-        payload.context = util.removeSensitiveData(payload.context);
-        payload.file = util.removeSensitiveData(payload.file);
-        payload.message = util.removeSensitiveData(payload.message);
-        payload.url = util.removeSensitiveData(payload.url);
-        payload.name = util.removeSensitiveData(payload.name);
-        payload.file = util.removeSensitiveData(payload.file);
+		payload.stacktrace = util.removeSensitiveData(payload.stacktrace);
+		payload.context = util.removeSensitiveData(payload.context);
+		payload.file = util.removeSensitiveData(payload.file);
+		payload.message = util.removeSensitiveData(payload.message);
+		payload.url = util.removeSensitiveData(payload.url);
+		payload.name = util.removeSensitiveData(payload.name);
+		payload.file = util.removeSensitiveData(payload.file);
 
-        for(var key in payload.metaData) {
-          payload.metaData[key] = util.removeSensitiveData(payload.metaData[key]);
-        }
-      };
-    }
+		for(let key in payload.metaData) {
+			payload.metaData[key] = util.removeSensitiveData(payload.metaData[key]);
+		}
+		};
+	}
   },
-  disableGlobalBackspace: function () {
-    document.onkeydown = function (e:any) {
-      e = e || window.event;
-      var doPrevent;
-      if (e.keyCode === 8) {
-        var d:any = e.srcElement || e.target;
-        if (d.tagName.toUpperCase() === 'INPUT' || d.tagName.toUpperCase() === 'TEXTAREA') {
-          doPrevent = d.readOnly || d.disabled;
-        } else {
-          doPrevent = true;
-        }
-      } else {
-        doPrevent = false;
-      }
-      if (doPrevent) {
-        e.preventDefault();
-      }
-    };
+  disableGlobalBackspace() {
+	document.onkeydown = function(e:any) {
+		e = e || window.event;
+		let doPrevent;
+		if (e.keyCode === 8) {
+		let d:any = e.srcElement || e.target;
+		if (d.tagName.toUpperCase() === "INPUT" || d.tagName.toUpperCase() === "TEXTAREA") {
+			doPrevent = d.readOnly || d.disabled;
+		} else {
+			doPrevent = true;
+		}
+		} else {
+		doPrevent = false;
+		}
+		if (doPrevent) {
+		e.preventDefault();
+		}
+	};
   },
 };
