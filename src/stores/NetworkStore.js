5a7e0737ca2325fed3b02d42836e4ce738af0e1d
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 17:59:06 +0200

    Tagg

--- src/stores/NetworkStore.js
+++ src/stores/NetworkStore.js
@@ -1,34 +1,28 @@
-import alt from '../renderer/alt';
-import networkActions from '../actions/NetworkActions';
-
+import networkActions from "../actions/NetworkActions";
+import alt from "../renderer/alt";
 class NetworkStore {
-  constructor () {
-    this.bindActions(networkActions);
-    this.networks = [];
-    this.pending = null;
-    this.error = null;
-  }
-
-  error (error) {
-    this.setState({error: error});
-  }
-
-  updated (networks) {
-    this.setState({error: null, networks: networks});
-  }
-
-  pending () {
-    this.setState({pending: true});
-  }
-
-  clearPending () {
-    this.setState({pending: null});
-  }
-
-  static all () {
-    let state = this.getState();
-    return state.networks;
-  }
+    static all() {
+        let state = this.getState();
+        return state.networks;
+    }
+    constructor() {
+        this.bindActions(networkActions);
+        this.networks = [];
+        this.pending = null;
+        this.error = null;
+    }
+    error(error) {
+        this.setState({ error });
+    }
+    updated(networks) {
+        this.setState({ error: null, networks });
+    }
+    pending() {
+        this.setState({ pending: true });
+    }
+    clearPending() {
+        this.setState({ pending: null });
+    }
 }
-
 export default alt.createStore(NetworkStore);
+//# sourceMappingURL=NetworkStore.js.map
--- src/stores/TagStore.js
+++ src/stores/TagStore.js
@@ -1,57 +1,48 @@
-import alt from '../renderer/alt';
-import tagActions from '../actions/TagActions';
-import tagServerActions from '../actions/TagServerActions';
-import accountServerActions from '../actions/AccountServerActions';
-
+import accountServerActions from "../actions/AccountServerActions";
+import tagActions from "../actions/TagActions";
+import tagServerActions from "../actions/TagServerActions";
+import alt from "../renderer/alt";
 class TagStore {
-  constructor () {
-    this.bindActions(tagActions);
-    this.bindActions(tagServerActions);
-    this.bindActions(accountServerActions);
-
-    // maps 'namespace/name' => [list of tags]
-    this.tags = {};
-
-    // maps 'namespace/name' => true / false
-    this.loading = {};
-  }
-
-  tags ({repo}) {
-    this.loading[repo] = true;
-    this.emitChange();
-  }
-
-  localTags ({repo, tags}) {
-    let data = [];
-    tags.map((value) => {
-      data.push({'name': value});
-    });
-    this.loading[repo] = true;
-    this.tagsUpdated({repo, tags: data || []});
-  }
-
-  tagsUpdated ({repo, tags}) {
-    this.tags[repo] = tags;
-    this.loading[repo] = false;
-    this.emitChange();
-  }
-
-  remove ({repo}) {
-    delete this.tags[repo];
-    delete this.loading[repo];
-    this.emitChange();
-  }
-
-  loggedout () {
-    this.loading = {};
-    this.tags = {};
-    this.emitChange();
-  }
-
-  error ({repo}) {
-    this.loading[repo] = false;
-    this.emitChange();
-  }
+    constructor() {
+        this.bindActions(tagActions);
+        this.bindActions(tagServerActions);
+        this.bindActions(accountServerActions);
+        // maps 'namespace/name' => [list of tags]
+        this.tags = {};
+        // maps 'namespace/name' => true / false
+        this.loading = {};
+    }
+    tags({ repo }) {
+        this.loading[repo] = true;
+        this.emitChange();
+    }
+    localTags({ repo, tags }) {
+        let data = [];
+        tags.map((value) => {
+            data.push({ name: value });
+        });
+        this.loading[repo] = true;
+        this.tagsUpdated({ repo, tags: data || [] });
+    }
+    tagsUpdated({ repo, tags }) {
+        this.tags[repo] = tags;
+        this.loading[repo] = false;
+        this.emitChange();
+    }
+    remove({ repo }) {
+        delete this.tags[repo];
+        delete this.loading[repo];
+        this.emitChange();
+    }
+    loggedout() {
+        this.loading = {};
+        this.tags = {};
+        this.emitChange();
+    }
+    error({ repo }) {
+        this.loading[repo] = false;
+        this.emitChange();
+    }
 }
-
 export default alt.createStore(TagStore);
+//# sourceMappingURL=TagStore.js.map
