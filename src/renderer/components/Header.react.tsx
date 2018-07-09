e552c8222899833edff17bdf6d68d5d9eb453904
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 16:46:28 +0200

    fixed bug

--- src/renderer/components/Header.react.tsx
+++ src/renderer/components/Header.react.tsx
@@ -1,61 +1,58 @@
-import classNames from "classnames";
 import {remote} from "electron";
-import {Component} from "react";
+import RetinaImage from "react-retina-image";
+import React from "react/addons";
+const Menu = remote.Menu;
+const MenuItem = remote.MenuItem;
+import classNames from "classnames";
 import Router from "react-router";
 import accountActions from "../../actions/AccountActions";
 import metrics from "../../utils/MetricsUtil";
 import util from "../../utils/Util";
-import {ImageResources} from "../resources/ImageResources";
 import accountStore from "../stores/AccountStore";
 
-export default class Header extends Component<HeaderProps, HeaderState> {
-
-	public mixins = [Router.Navigation];
-
-	public constructor(props) {
-		super(props);
-		this.state = new HeaderState();
-		this.update = this.update.bind(this);
-	}
-
-	public componentDidMount() {
+export default React.createClass({
+	mixins: [Router.Navigation],
+	getInitialState() {
+		return {
+			fullscreen: false,
+			updateAvailable: false,
+			username: accountStore.getState().username,
+			verified: accountStore.getState().verified,
+		};
+	},
+	componentDidMount() {
 		document.addEventListener("keyup", this.handleDocumentKeyUp, false);
-		accountStore.listen(this.update);
-	}
 
-	public componentWillUnmount() {
+		accountStore.listen(this.update);
+	},
+	componentWillUnmount() {
 		document.removeEventListener("keyup", this.handleDocumentKeyUp, false);
 		accountStore.unlisten(this.update);
-	}
-
-	public update() {
-		const accountState = accountStore.getState();
+	},
+	update() {
+		let accountState = accountStore.getState();
 		this.setState({
 			username: accountState.username,
 			verified: accountState.verified,
 		});
-	}
-
-	public handleDocumentKeyUp(e) {
+	},
+	handleDocumentKeyUp(e) {
 		if (e.keyCode === 27 && remote.getCurrentWindow().isFullScreen()) {
 			remote.getCurrentWindow().setFullScreen(false);
 			this.forceUpdate();
 		}
-	}
-
-	public handleClose() {
+	},
+	handleClose() {
 		if (util.isWindows() || util.isLinux()) {
 			remote.getCurrentWindow().close();
 		} else {
 			remote.getCurrentWindow().hide();
 		}
-	}
-
-	public handleMinimize() {
+	},
+	handleMinimize() {
 		remote.getCurrentWindow().minimize();
-	}
-
-	public handleFullscreen() {
+	},
+	handleFullscreen() {
 		if (util.isWindows()) {
 			if (remote.getCurrentWindow().isMaximized()) {
 				remote.getCurrentWindow().unmaximize();
@@ -71,71 +68,57 @@ export default class Header extends Component<HeaderProps, HeaderState> {
 				fullscreen: remote.getCurrentWindow().isFullScreen(),
 			});
 		}
-	}
-
-	public handleFullscreenHover() {
+	},
+	handleFullscreenHover() {
 		this.update();
-	}
+	},
+	handleUserClick(e) {
+		let menu = new Menu();
 
-	public handleUserClick(e) {
-		const menu = new remote.Menu();
 		if (!this.state.verified) {
-			menu.append(new remote.MenuItem({label: "I've Verified My Email Address", click: this.handleVerifyClick}));
+			menu.append(new MenuItem({ label: "I've Verified My Email Address", click: this.handleVerifyClick}));
 		}
 
-		menu.append(new remote.MenuItem({
-			label: "Sign Out",
-			click: this.handleLogoutClick,
-		}));
+		menu.append(new MenuItem({ label: "Sign Out", click: this.handleLogoutClick}));
 		menu.popup({
 			window: remote.getCurrentWindow(),
 			x: e.currentTarget.offsetLeft,
 			y: e.currentTarget.offsetTop + e.currentTarget.clientHeight + 10,
 		});
-	}
-
-	public handleLoginClick(event) {
-		(this as any).transitionTo("login");
+	},
+	handleLoginClick() {
+		this.transitionTo("login");
 		metrics.track("Opened Log In Screen");
-	}
-
-	public handleLogoutClick() {
+	},
+	handleLogoutClick() {
 		metrics.track("Logged Out");
 		accountActions.logout();
-	}
-
-	public handleVerifyClick() {
+	},
+	handleVerifyClick() {
 		metrics.track("Verified Account", {
 			from: "header",
 		});
 		accountActions.verify();
-	}
-
-	public renderLogo() {
+	},
+	renderLogo() {
 		return (
 			<div className="logo">
-				<img src={ImageResources.LOGO} width={40} height={32}/>
+				<RetinaImage src="logo.png"/>
 			</div>
 		);
-	}
-
-	public renderWindowButtons() {
+	},
+	renderWindowButtons() {
+		let buttons;
 		if (util.isWindows()) {
-			return (
+			buttons = (
 				<div className="windows-buttons">
-					<div className="windows-button button-minimize enabled" onClick={this.handleMinimize}>
-						<div className="icon"></div>
-					</div>
-					<div
-						className={`windows-button ${(this as any).state.fullscreen ? "button-fullscreenclose" : "button-fullscreen"} enabled`}
-						onClick={this.handleFullscreen}>
-						<div className="icon"></div>
-					</div>
+					<div className="windows-button button-minimize enabled" onClick={this.handleMinimize}><div className="icon"></div></div>
+					<div className={`windows-button ${this.state.fullscreen ? "button-fullscreenclose" : "button-fullscreen"} enabled`} onClick={this.handleFullscreen}><div className="icon"></div></div>
 					<div className="windows-button button-close enabled" onClick={this.handleClose}></div>
 				</div>
 			);
 		} else {
-			return  (
+			buttons = (
 				<div className="buttons">
 					<div className="button button-close enabled" onClick={this.handleClose}></div>
 					<div className="button button-minimize enabled" onClick={this.handleMinimize}></div>
@@ -143,10 +126,10 @@ export default class Header extends Component<HeaderProps, HeaderState> {
 				</div>
 			);
 		}
-	}
-
-	public renderDashboardHeader() {
-		const headerClasses = classNames({
+		return buttons;
+	},
+	renderDashboardHeader() {
+		let headerClasses = classNames({
 			"bordered": !this.props.hideLogin,
 			"header": true,
 			"no-drag": true,
@@ -160,10 +143,10 @@ export default class Header extends Component<HeaderProps, HeaderState> {
 					<div className="login no-drag" onClick={this.handleUserClick}>
 						<span className="icon icon-user"></span>
 						<span className="text">
-				{(this as any).state.username}
-							{(this as any).state.verified ? null : "(Unverified)"}
+				{this.state.username}
+							{this.state.verified ? null : "(Unverified)"}
 				</span>
-						<img src={ImageResources.USER_DROP_DOWN}/>
+						<RetinaImage src="userdropdown.png"/>
 					</div>
 				</div>
 			);
@@ -179,18 +162,17 @@ export default class Header extends Component<HeaderProps, HeaderState> {
 		return (
 			<div className={headerClasses}>
 				<div className="left-header">
-					{util.isWindows() ? this.renderLogo() : this.renderWindowButtons()}
+					{util.isWindows () ? this.renderLogo() : this.renderWindowButtons()}
 					{username}
 				</div>
 				<div className="right-header">
-					{util.isWindows() ? this.renderWindowButtons() : this.renderLogo()}
+					{util.isWindows () ? this.renderWindowButtons() : this.renderLogo()}
 				</div>
 			</div>
 		);
-	}
-
-	public renderBasicHeader() {
-		const headerClasses = classNames({
+	},
+	renderBasicHeader() {
+		let headerClasses = classNames({
 			"bordered": !this.props.hideLogin,
 			"header": true,
 			"no-drag": true,
@@ -198,39 +180,19 @@ export default class Header extends Component<HeaderProps, HeaderState> {
 		return (
 			<div className={headerClasses}>
 				<div className="left-header">
-					{util.isWindows() ? null : this.renderWindowButtons()}
+					{util.isWindows () ? null : this.renderWindowButtons()}
 				</div>
 				<div className="right-header">
-					{util.isWindows() ? this.renderWindowButtons() : null}
+					{util.isWindows () ? this.renderWindowButtons() : null}
 				</div>
 			</div>
 		);
-	}
-
-	public render() {
+	},
+	render() {
 		if (this.props.hideLogin) {
 			return this.renderBasicHeader();
 		} else {
 			return this.renderDashboardHeader();
 		}
-	}
-
-}
-
-export class HeaderProps {
-
-	public hideLogin: boolean;
-
-}
-
-export class HeaderState {
-
-	public fullscreen = false;
-
-	public updateAvailable = false;
-
-	public username = accountStore.getState().username;
-
-	public verified = accountStore.getState().verified;
-
-}
+	},
+});
