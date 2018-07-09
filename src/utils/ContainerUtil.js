0f3a338d7dc5f310c1405cfe73287d584ab6271b
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 18:53:33 +0200

    ContainerUtil

--- src/utils/ContainerUtil.js
+++ src/utils/ContainerUtil.js
@@ -1,76 +1,64 @@
-import _ from 'underscore';
-import docker from './DockerUtil';
-
+import _ from "underscore";
+import docker from "./DockerUtil";
 export default {
-  env: function (container) {
-    if (!container || !container.Config || !container.Config.Env) {
-      return [];
-    }
-    return _.map(container.Config.Env, env => {
-      var i = env.indexOf('=');
-      var splits = [env.slice(0, i), env.slice(i + 1)];
-      return splits;
-    });
-  },
-
-  // Provide Foreground options
-  mode: function (container) {
-    return [
-        (container && container.Config) ? container.Config.Tty : true,
-        (container && container.Config) ? container.Config.OpenStdin : true,
-        (container && container.HostConfig) ? container.HostConfig.Privileged : false,
-        (container && container.HostConfig) ? container.HostConfig.RestartPolicy : {MaximumRetryCount: 0, Name: 'no'}
-    ];
-  },
-
-  // TODO: inject host here instead of requiring Docker
-  ports: function (container) {
-    if (!container || !container.NetworkSettings) {
-      return {};
-    }
-    var res = {};
-    var ip = docker.host;
-    var ports = (container.NetworkSettings.Ports) ? container.NetworkSettings.Ports : ((container.HostConfig.PortBindings) ? container.HostConfig.PortBindings : container.Config.ExposedPorts);
-    _.each(ports, function (value, key) {
-      var [dockerPort, portType] = key.split('/');
-      var localUrl = null;
-      var port = null;
-      if (value && value.length) {
-        port = value[0].HostPort;
-      }
-      localUrl = (port) ? ip + ':' + port : ip + ':' + '<not set>';
-
-      res[dockerPort] = {
-        url: localUrl,
-        ip: ip,
-        port: port,
-        portType: portType
-      };
-    });
-    return res;
-  },
-
-  links: function (container) {
-    if (!container || !container.HostConfig || !container.HostConfig.Links) {
-      return [];
-    }
-
-    var res = _.map(container.HostConfig.Links, (link, key) => {
-        return {
-            "container": link.split(":")[0].split("/")[1],
-            "alias": link.split(":")[1].split("/")[2],
+    env(container) {
+        if (!container || !container.Config || !container.Config.Env) {
+            return [];
         }
-    });
-
-    return res;
-  },
-
-  normalizeLinksPath: function (container, links) {
-    var res = _.map(links, (link) => {
-      return "/"+link.container+":/"+container.Name+"/"+link.alias;
-    });
-
-    return res;
-  }
-
+        return _.map(container.Config.Env, (env) => {
+            let i = env.indexOf("=");
+            return [env.slice(0, i), env.slice(i + 1)];
+        });
+    },
+    // Provide Foreground options
+    mode(container) {
+        return [
+            (container && container.Config) ? container.Config.Tty : true,
+            (container && container.Config) ? container.Config.OpenStdin : true,
+            (container && container.HostConfig) ? container.HostConfig.Privileged : false,
+            (container && container.HostConfig) ? container.HostConfig.RestartPolicy : { MaximumRetryCount: 0, Name: "no" },
+        ];
+    },
+    // TODO: inject host here instead of requiring Docker
+    ports(container) {
+        if (!container || !container.NetworkSettings) {
+            return {};
+        }
+        let res = {};
+        let ip = docker.host;
+        let ports = (container.NetworkSettings.Ports) ? container.NetworkSettings.Ports : ((container.HostConfig.PortBindings) ? container.HostConfig.PortBindings : container.Config.ExposedPorts);
+        _.each(ports, function (value, key) {
+            let [dockerPort, portType] = key.split("/");
+            let localUrl = null;
+            let port = null;
+            if (value && value.length) {
+                port = value[0].HostPort;
+            }
+            localUrl = (port) ? ip + ":" + port : ip + ":" + "<not set>";
+            res[dockerPort] = {
+                url: localUrl,
+                ip,
+                port,
+                portType,
+            };
+        });
+        return res;
+    },
+    links(container) {
+        if (!container || !container.HostConfig || !container.HostConfig.Links) {
+            return [];
+        }
+        return _.map(container.HostConfig.Links, (link, key) => {
+            return {
+                container: link.split(":")[0].split("/")[1],
+                alias: link.split(":")[1].split("/")[2],
+            };
+        });
+    },
+    normalizeLinksPath(container, links) {
+        return _.map(links, (link) => {
+            return "/" + link.container + ":/" + container.Name + "/" + link.alias;
+        });
+    },
 };
+//# sourceMappingURL=ContainerUtil.js.map
---
+++ src/utils/ContainerUtil.ts
@@ -0,0 +1,71 @@
+import _ from "underscore";
+import docker from "./DockerUtil";
+
+export default {
+	env(container) {
+		if (!container || !container.Config || !container.Config.Env) {
+			return [];
+		}
+		return _.map(container.Config.Env, (env) => {
+			let i = env.indexOf("=");
+			return [env.slice(0, i), env.slice(i + 1)];
+		});
+	},
+
+	// Provide Foreground options
+	mode(container) {
+		return [
+			(container && container.Config) ? container.Config.Tty : true,
+			(container && container.Config) ? container.Config.OpenStdin : true,
+			(container && container.HostConfig) ? container.HostConfig.Privileged : false,
+			(container && container.HostConfig) ? container.HostConfig.RestartPolicy : {MaximumRetryCount: 0, Name: "no"},
+		];
+	},
+
+	// TODO: inject host here instead of requiring Docker
+	ports(container) {
+		if (!container || !container.NetworkSettings) {
+			return {};
+		}
+		let res = {};
+		let ip = docker.host;
+		let ports = (container.NetworkSettings.Ports) ? container.NetworkSettings.Ports : ((container.HostConfig.PortBindings) ? container.HostConfig.PortBindings : container.Config.ExposedPorts);
+		_.each(ports, function(value, key) {
+			let [dockerPort, portType] = key.split("/");
+			let localUrl = null;
+			let port = null;
+			if (value && value.length) {
+				port = value[0].HostPort;
+			}
+			localUrl = (port) ? ip + ":" + port : ip + ":" + "<not set>";
+
+			res[dockerPort] = {
+				url: localUrl,
+				ip,
+				port,
+				portType,
+			};
+		});
+		return res;
+	},
+
+	links(container) {
+		if (!container || !container.HostConfig || !container.HostConfig.Links) {
+			return [];
+		}
+
+		return _.map(container.HostConfig.Links, (link, key) => {
+			return {
+				container: link.split(":")[0].split("/")[1],
+				alias: link.split(":")[1].split("/")[2],
+			};
+		});
+	},
+
+	normalizeLinksPath(container, links) {
+		return _.map(links, (link) => {
+			return "/" + link.container + ":/" + container.Name + "/" + link.alias;
+		});
+	},
+
+};
