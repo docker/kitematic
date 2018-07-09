14915cfeb2e653bfc97d907c166016f03a55f918
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 18:00:47 +0200

    Container Details

--- src/components/ContainerDetails.react.tsx
+++ src/components/ContainerDetails.react.tsx
@@ -5,23 +5,28 @@ import containerUtil from "../utils/ContainerUtil";
 import util from "../utils/Util";
 import ContainerDetailsHeader from "./ContainerDetailsHeader.react.jsx";
 import ContainerDetailsSubheader from "./ContainerDetailsSubheader.react.jsx";
+
 export default React.createClass({
-    contextTypes: {
-        router: React.PropTypes.func,
-    },
-    render() {
-        if (!this.props.container) {
-            return false;
-        }
-        let ports = containerUtil.ports(this.props.container);
-        let defaultPort = _.find(_.keys(ports), (port) => {
-            return util.webPorts.indexOf(port) !== -1;
-        });
-        return (<div className="details">
+  contextTypes: {
+	router: React.PropTypes.func,
+  },
+
+  render() {
+	if (!this.props.container) {
+		return false;
+	}
+
+	let ports = containerUtil.ports(this.props.container);
+	let defaultPort = _.find(_.keys(ports), (port) => {
+		return util.webPorts.indexOf(port) !== -1;
+	});
+
+	return (
+		<div className="details">
 		<ContainerDetailsHeader {...this.props} defaultPort={defaultPort} ports={ports}/>
 		<ContainerDetailsSubheader {...this.props} defaultPort={defaultPort} ports={ports}/>
 		<Router.RouteHandler {...this.props} defaultPort={defaultPort} ports={ports}/>
-		</div>);
-    },
+		</div>
+	);
+  },
 });
-//# sourceMappingURL=ContainerDetails.react.jsx.map
