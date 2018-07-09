7d55ab83b0972bed6d36286099bafd3a98dd1ee3
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 17:39:16 +0200

    ImageActions

--- src/actions/ContainerActions.js
+++ src/actions/ContainerActions.js
@@ -1,59 +1,48 @@
-import alt from '../renderer/alt';
-import dockerUtil from '../utils/DockerUtil';
-import _ from "underscore";
-
+import alt from "../renderer/alt";
+import dockerUtil from "../utils/DockerUtil";
 class ContainerActions {
-
-  destroy (name) {
-    dockerUtil.destroy(name);
-  }
-
-  rename (name, newName) {
-    this.dispatch({name, newName});
-    dockerUtil.rename(name, newName);
-  }
-
-  start (name) {
-    this.dispatch({name});
-    dockerUtil.start(name);
-  }
-
-  stop (name) {
-    dockerUtil.stop(name);
-  }
-
-  restart (name) {
-    this.dispatch({name});
-    dockerUtil.restart(name);
-  }
-
-  update (name, container) {
-    this.dispatch({name, container});
-    dockerUtil.updateContainer(name, container);
-  }
-
-  clearPending () {
-    this.dispatch();
-  }
-
-  run (name, repo, tag, network, local=false) {
-    dockerUtil.run(name, repo, tag, network, local);
-  }
-
-  active (name) {
-    dockerUtil.active(name);
-  }
-
-  toggleFavorite (name) {
-    let favorites = JSON.parse(localStorage.getItem('containers.favorites')) || [];
-    if (favorites.includes(name)) {
-      favorites = favorites.filter(favoriteName => favoriteName !== name);
-    } else {
-      favorites = [...favorites, name];
+    destroy(name) {
+        dockerUtil.destroy(name);
     }
-    localStorage.setItem('containers.favorites', JSON.stringify(favorites));
-    this.dispatch({name});
-  }
+    rename(name, newName) {
+        this.dispatch({ name, newName });
+        dockerUtil.rename(name, newName);
+    }
+    start(name) {
+        this.dispatch({ name });
+        dockerUtil.start(name);
+    }
+    stop(name) {
+        dockerUtil.stop(name);
+    }
+    restart(name) {
+        this.dispatch({ name });
+        dockerUtil.restart(name);
+    }
+    update(name, container) {
+        this.dispatch({ name, container });
+        dockerUtil.updateContainer(name, container);
+    }
+    clearPending() {
+        this.dispatch();
+    }
+    run(name, repo, tag, network, local = false) {
+        dockerUtil.run(name, repo, tag, network, local);
+    }
+    active(name) {
+        dockerUtil.active(name);
+    }
+    toggleFavorite(name) {
+        let favorites = JSON.parse(localStorage.getItem("containers.favorites")) || [];
+        if (favorites.includes(name)) {
+            favorites = favorites.filter((favoriteName) => favoriteName !== name);
+        }
+        else {
+            favorites = [...favorites, name];
+        }
+        localStorage.setItem("containers.favorites", JSON.stringify(favorites));
+        this.dispatch({ name });
+    }
 }
-
 export default alt.createActions(ContainerActions);
+//# sourceMappingURL=ContainerActions.js.map
--- src/actions/ImageActions.js
+++ src/actions/ImageActions.js
@@ -1,16 +1,13 @@
-import alt from '../renderer/alt';
-import dockerUtil from '../utils/DockerUtil';
-
+import alt from "../renderer/alt";
+import dockerUtil from "../utils/DockerUtil";
 class ImageActions {
-
-  all () {
-    this.dispatch({});
-    dockerUtil.refresh();
-  }
-
-  destroy (image) {
-    dockerUtil.removeImage(image);
-  }
+    all() {
+        this.dispatch({});
+        dockerUtil.refresh();
+    }
+    destroy(image) {
+        dockerUtil.removeImage(image);
+    }
 }
-
 export default alt.createActions(ImageActions);
+//# sourceMappingURL=ImageActions.js.map
