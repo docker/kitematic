25b274a4eb2676a7df203e88ec8a01838ad9db77
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 18:48:11 +0200

    semi

---
+++ src/stores/SetupStore.ts
@@ -0,0 +1,29 @@
+import setupActions from "../actions/SetupActions";
+import setupServerActions from "../actions/SetupServerActions";
+import alt from "../renderer/alt";
+
+class SetupStore {
+
+	public constructor() {
+		(this as any).bindActions(setupActions);
+		(this as any).bindActions(setupServerActions);
+		(this as any).started = false;
+		(this as any).progress = null;
+		(this as any).error = null;
+	}
+
+	public started({started}) {
+		(this as any).setState({error: null, started});
+	}
+
+	public error({error}) {
+		(this as any).setState({error, progress: null});
+	}
+
+	public progress({progress}) {
+		(this as any).setState({progress});
+	}
+
+}
+
+export default alt.createStore(SetupStore);
