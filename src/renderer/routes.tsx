a817276a1a2e1efa6ad96cb67123ae2d2c0fd346
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-09 17:08:02 +0200

    About

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -1,6 +1,5 @@
 import {Component} from "react";
 import Router from "react-router";
-import About from "../components/About.react.jsx";
 import Account from "../components/Account.react.jsx";
 import AccountLogin from "../components/AccountLogin.react.jsx";
 import AccountSignup from "../components/AccountSignup.react.jsx";
@@ -16,6 +15,7 @@ import ContainerSettingsVolumes from "../components/ContainerSettingsVolumes.rea
 import NewContainerSearch from "../components/NewContainerSearch.react.jsx";
 import Preferences from "../components/Preferences.react.jsx";
 import Setup from "../components/Setup.react.jsx";
+import About from "./components/About.react.jsx";
 import Loading from "./components/Loading.react.jsx";
 
 export class App extends Component {
