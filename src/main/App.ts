9b64ec48741fc9e46dd908c882636efcc1cb7867
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-10 18:20:05 +0200

    .

---
+++ src/main/App.ts
@@ -0,0 +1,59 @@
+import {app, BrowserWindow} from "electron";
+import {readFileSync} from "fs";
+import {platform} from "os";
+import {join} from "path";
+import {FileResources} from "../common/resources/FileResources";
+import {Settings} from "./Settings";
+
+export class App {
+
+	public static onReady() {
+		let size: {height, width} = new Settings();
+		try {
+			size = JSON.parse(readFileSync(join(app.getPath("userData"), "size.json"), "utf8"));
+		} catch (err) {}
+
+		const mainWindow = new BrowserWindow({
+			frame: false,
+			height: size.height,
+			minHeight: platform() === "win32" ? 260 : 500,
+			minWidth: platform() === "win32" ? 400 : 700,
+			resizable: true,
+			show: false,
+			width: size.width,
+		});
+
+		if (process.env.NODE_ENV === "development") {
+			mainWindow.webContents.openDevTools({
+				mode: "detach",
+			});
+		}
+
+		mainWindow.loadFile(FileResources.INDEX_HTML);
+
+		app.on("activate", () => {
+			mainWindow.show();
+		});
+
+		mainWindow.on("close", (event: Event) => {
+			mainWindow.webContents.send("application:quitting");
+		});
+
+		mainWindow.webContents.on("new-window", (event: Event) => {
+			event.preventDefault();
+		});
+
+		mainWindow.webContents.on("will-navigate", (event: Event, url: string) => {
+			if (url.indexOf("build/index.html#") < 0) {
+				event.preventDefault();
+			}
+		});
+
+		mainWindow.webContents.on("did-finish-load", () => {
+			mainWindow.setTitle("Kitematic");
+			mainWindow.show();
+			mainWindow.focus();
+		});
+	}
+
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,6 @@
-import {app, BrowserWindow} from "electron";
-import {readFileSync} from "fs";
-import {platform} from "os";
-import {join} from "path";
+import {app} from "electron";
 import {FileResources} from "../common/resources/FileResources";
-import {Settings} from "./Settings";
+import {App} from "./App";
 
 process.env.NODE_PATH = FileResources.NODE_MODULES_PATH;
 process.env.RESOURCES_PATH = FileResources.RESOURCES_PATH;
@@ -11,51 +8,4 @@ if (process.platform !== "win32") {
 	process.env.PATH = "/usr/local/bin:" + process.env.PATH;
 }
 
-let size: {height, width} = new Settings();
-try {
-	size = JSON.parse(readFileSync(join(app.getPath("userData"), "size.json"), "utf8"));
-} catch (err) {}
-
-app.on("ready", () => {
-	const mainWindow = new BrowserWindow({
-		frame: false,
-		height: size.height,
-		minHeight: platform() === "win32" ? 260 : 500,
-		minWidth: platform() === "win32" ? 400 : 700,
-		resizable: true,
-		show: false,
-		width: size.width,
-	});
-
-	if (process.env.NODE_ENV === "development") {
-		mainWindow.webContents.openDevTools({
-			mode: "detach",
-		});
-	}
-
-	mainWindow.loadFile(FileResources.INDEX_HTML);
-
-	app.on("activate", () => {
-		mainWindow.show();
-	});
-
-	mainWindow.on("close", (event: Event) => {
-		mainWindow.webContents.send("application:quitting");
-	});
-
-	mainWindow.webContents.on("new-window", (event: Event) => {
-		event.preventDefault();
-	});
-
-	mainWindow.webContents.on("will-navigate", (event: Event, url: string) => {
-		if (url.indexOf("build/index.html#") < 0) {
-			event.preventDefault();
-		}
-	});
-
-	mainWindow.webContents.on("did-finish-load", () => {
-		mainWindow.setTitle("Kitematic");
-		mainWindow.show();
-		mainWindow.focus();
-	});
-});
+app.on("ready", App.onReady);
